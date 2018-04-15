7a7a72263ceec65b7058829ee503b5ce6312e59b
yukap <wb-jwr216045@alibaba-inc.com>
2018-04-15 22:32:22 +0800

    insert sort add

--- test.js
+++ test.js
@@ -1,5 +1,4 @@
-const _ = require('lodash');
-
+// 二分查找
 function binaryChop(search, targetArray) {
   let low = 0;
   let hig = targetArray.length - 1;
@@ -32,5 +31,24 @@ function selectSort(arr) {
   }
   return arr;
 }
+
+function less(a, b) {
+  return a - b < 0;
+}
+function exch(arr, indexA, indexB) {
+  let tmp = arr[indexA];
+  arr[indexA] = arr[indexB];
+  arr[indexB] = tmp;
+}
+
+// 插入排序
+function insertSort(arr) {
+  for (let i = 0, len = arr.length; i < len; i++) {
+    for (let j = i; j > 0 && less(arr[j], arr[j - 1]); j--) {
+      exch(arr, j, j - 1);
+    }
+  }
+  return arr;
+}
 const arr = [19,28,98, 1,2,3,0,1,2,3,6,7,8,9,101,102];
-console.log(selectSort(arr));
+console.log(insertSort(arr));
